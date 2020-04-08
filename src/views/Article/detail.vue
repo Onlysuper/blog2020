c12cbb727d3f8f0c7dc9e0834d4ffd1714f35896
Onlysuper <2483613372@qq.com>
2020-04-08 17:59:17 +0800

    合伙人”推广码“改为”注册码“

--- src/views/Article/detail.vue
+++ src/views/Article/detail.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="container">
-        我是详情
-    </div>
+    <article class="container">
+        <div class="post-card">
+            <h1>小技巧|使用Vue.js的Mixins复用你的代码</h1>
+            
+        </div>
+    </article>
 </template>
 <script>
 export default {
