018d0a3c4a55595e255dde18bd63c783fc5f26d1
Onlysuper <2483613372@qq.com>
2020-04-10 15:19:14 +0800

    内容编辑

--- src/views/Article/detail.vue
+++ src/views/Article/detail.vue
@@ -1,203 +1,183 @@
 <template>
-    <article class="articledetail-page">
-       <div class="detail-inner width-limit">
-            <div class="detail-card">
-             <div class="content bs-outline">
-                <h1>小技巧|使用Vue.js的Mixins复用你的代码</h1>
-                <div class="meta">
-                    <time>2019-01-02</time>
-                </div>
-                <p>Vue中的混入 mixins 是一种提供分发 Vue 组件中可复用功能的非常灵活的方式。听说在3.0版本中可能会用Hooks的形式实现，但这并不妨碍它的强大。基础部分的可以看这里。</p>
-                <p>这里主要来讨论 mixins 如何优化我们的数据列表代码。</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <h2 id="1-意外全局变量">意外全局变量</h2>
-                <p>Vue中的混入 mixins 是一种提供分发 Vue 组件中可复用功能的非常灵活的方式。听说在3.0版本中可能会用Hooks的形式实现，但这并不妨碍它的强大。基础部分的可以看这里。</p>
-                <p>这里主要来讨论 mixins 如何优化我们的数据列表代码。</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <h3 id="Question">Question</h3>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
-                    <!-- </div> -->
-                <div class="code_content" v-highlight>
-                    <div v-html="input"></div>
-                </div>
-                <!-- </div> -->
-                <div class="comments">
-                    <!-- <h3>我要评论</h3> -->
-                    <div class="comment-inner">
-                        <input placeholder="我要留言" />
-                        <button class="bs-button">
-                            <span class="icon iconfont icon-fasong"></span>
-                        </button>
-                            <!-- <mavon-editor 
-                            v-model="commentContent" 
-                            ref="md" 
-                            @change="changeComment" 
-                            style="min-height: 600px"
-                        /> -->
-                    </div>
-                    
-                </div>
-             </div>
+  <article class="articledetail-page">
+    <div class="detail-inner width-limit">
+      <div class="detail-card">
+        <div class="content bs-outline">
+          <h1>小技巧|使用Vue.js的Mixins复用你的代码</h1>
+          <div class="meta">
+            <time>2019-01-02</time>
+          </div>
+          <p>Vue中的混入 mixins 是一种提供分发 Vue 组件中可复用功能的非常灵活的方式。听说在3.0版本中可能会用Hooks的形式实现，但这并不妨碍它的强大。基础部分的可以看这里。</p>
+          <p>这里主要来讨论 mixins 如何优化我们的数据列表代码。</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <h2 id="1-意外全局变量">意外全局变量</h2>
+          <p>Vue中的混入 mixins 是一种提供分发 Vue 组件中可复用功能的非常灵活的方式。听说在3.0版本中可能会用Hooks的形式实现，但这并不妨碍它的强大。基础部分的可以看这里。</p>
+          <p>这里主要来讨论 mixins 如何优化我们的数据列表代码。</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <h3 id="Question">Question</h3>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <p>如果我们有大量的表格页面，仔细一扒拉你发现非常多的东西都是可以复用的例如分页，表格高度，加载方法， laoding 声明等一大堆的东西。下面我们来整理出来一个简单通用混入 list.js</p>
+          <!-- </div> -->
+          <div class="code_content" v-highlight>
+            <div v-html="input"></div>
+          </div>
+          <!-- </div> -->
+          <div class="comments">
+            <!-- <h3>我要评论</h3> -->
+            <div class="comment-inner">
+              <input placeholder="我要留言" />
+              <button class="bs-button">
+                <span class="icon iconfont icon-fasong"></span>
+              </button>
+
             </div>
-            <aside>
-                <nav class="top-warp bs-outline">
-                    <h4>TOC</h4>
-                    <ol>
-                        <li>
-                            <a href="#1-意外全局变量">1-意外全局变量</a>
-                            <ol>
-                            <li> 
-                                    <a href="#Question">
-                                    Question
-                                </a>
-                            </li>
-                            </ol>
-                        </li>
-                    </ol>
-                </nav>
-            </aside>
-       </div>
-    </article>
+
+          </div>
+        </div>
+      </div>
+      <aside>
+        <nav class="top-warp bs-outline">
+          <h4>TOC</h4>
+          <ol>
+            <li>
+              <a href="#1-意外全局变量">1-意外全局变量</a>
+              <ol>
+                <li>
+                  <a href="#Question">
+                    Question
+                  </a>
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </nav>
+      </aside>
+    </div>
+  </article>
 </template>
 <script>
 // 导入组件 及 组件样式
-import { mavonEditor } from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
+
 export default {
-    name:"Articledetail",
-    components: {
-        mavonEditor,
-    },
-    data(){
-        return {
-            commentContent:'', // 输入的markdown
-            conmentHtml:'',    // 及时转的html
-            input:'<pre class="hljs xml"><code style="background-color: transparent;"><span class="diff"></span><span class="line" data-start="0" data-start-original="1" data-end="1" data-id="1640223"></span><span class="hljs-tag">&lt;<span class="hljs-name">script</span>&gt;</span><span class="actionscript">'
-                +'</span><span class="line" data-start="2" data-end="2" data-id="1640223"></span><span class="actionscript">    <span class="hljs-keyword">var</span> name = <span class="hljs-string">"小名"</span>'
-+'</span><span class="line" data-start="3" data-end="3" data-id="1640223"></span><span class="hljs-tag">&lt;/<span class="hljs-name">script</span>&gt;</span></code></pre>'
-        }
-    },
-    methods:{
-        // 所有操作都会被解析重新渲染
-        changeComment(value, render){
-            // render 为 markdown 解析后的结果[html]
-            this.conmentHtml = render;
-        },
-        // 提交
-        submitComment(){
-            console.log(this.commentContent);
-            console.log(this.conmentHtml);
-        }
+  name: "Articledetail",
+
+  data() {
+    return {
+
+      input: '<pre class="hljs xml"><code style="background-color: transparent;"><span class="diff"></span><span class="line" data-start="0" data-start-original="1" data-end="1" data-id="1640223"></span><span class="hljs-tag">&lt;<span class="hljs-name">script</span>&gt;</span><span class="actionscript">'
+        + '</span><span class="line" data-start="2" data-end="2" data-id="1640223"></span><span class="actionscript">    <span class="hljs-keyword">var</span> name = <span class="hljs-string">"小名"</span>'
+        + '</span><span class="line" data-start="3" data-end="3" data-id="1640223"></span><span class="hljs-tag">&lt;/<span class="hljs-name">script</span>&gt;</span></code></pre>'
     }
+  }
 }
 </script> 
 <style lang="scss" scoped>
- // 内容样式
-.articledetail-page{
+// 内容样式
+.articledetail-page {
+  width: 100%;
+  .detail-inner {
+    display: flex;
+  }
+  .detail-card {
+    // padding-top: 20px;
+    position: relative;
+    width: 80%;
+    flex: 1;
+    box-sizing: border-box;
+    height: auto;
+    float: left;
+    &::after {
+      content: "";
+      display: block;
+      clear: both;
+    }
+    h2 {
+      font-size: 32px;
+    }
+    .meta {
+      time {
+        line-height: 14px;
+        font-size: 13px;
+        font-weight: bold;
+        color: #727272;
+      }
+    }
+    .content {
+      margin: 10px;
+      // margin-top: px;
+      box-sizing: border-box;
+      padding: 10px;
+      p {
+        margin-bottom: 30px;
+      }
+    }
+  }
+  aside {
+    width: 200px;
+    min-width: 200px;
+    flex: 0 0 200px;
+    float: left;
+    padding-top: 20px;
+    .top-warp {
+      position: fixed;
+      margin-left: 20px;
+      min-width: 200px;
+      box-sizing: border-box;
+      padding: 10px;
+      &.fixed {
+        top: 76px;
+        bottom: 140px;
+        overflow-y: auto;
+      }
+      ol,
+      li {
+        list-style: none;
+        a {
+          // color
+        }
+      }
+    }
+  }
+  .comments {
+    margin-top: 30px;
     width: 100%;
-    .detail-inner{
-        display: flex;
-    }
-    .detail-card{
-        // padding-top: 20px;
-        position: relative;
-        width: 80%;
+    .comment-inner {
+      display: flex;
+      flex-direction: row;
+      input {
         flex: 1;
-        box-sizing: border-box;
-        height: auto;
-        float: left;
-        &::after{
-            content: '';
-            display: block;
-             clear:both;
-        }
-        h2{
-            font-size: 32px;
-        }
-        .meta{
-            time{
-                line-height: 14px;
-                font-size: 13px;
-                font-weight: bold;
-                color: #727272;
-            }
-        }
-        .content{
-            margin: 10px;
-            // margin-top: px;
-            box-sizing: border-box;
-            padding: 10px;
-            p{
-                margin-bottom: 30px;
-            }
-        }
+      }
+      button {
+        flex: 0 0 1;
+        margin-left: 20px;
+        width: 40px;
+        height: 40px;
+      }
     }
-    aside{
-        width:200px;
-        min-width: 200px;
-        flex: 0 0 200px;
-        float: left;
-        padding-top: 20px;
-        .top-warp{
-            position: fixed;
-            margin-left:20px;
-            min-width: 200px;
-            box-sizing: border-box;
-            padding: 10px;
-            &.fixed{
-                top: 76px;
-                bottom: 140px;
-                overflow-y: auto;
-            }
-            ol,li{
-                list-style: none;
-                a{
-                    // color
-                }
-            }
-        }
+
+    .footer {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
     }
-    .comments{
-        margin-top: 30px;
-        width: 100%;
-        .comment-inner{
-            display: flex;
-            flex-direction: row;
-            input{
-                flex: 1;
-            }
-            button{
-                flex: 0 0 1;
-                margin-left: 20px;
-                width: 40px;
-                height: 40px;
-            }
-        }
-        
-        .footer{
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-end;
-        }
-    }
+  }
 }
 
-@media screen and (max-width: 760px){
-    .articledetail-page{
-        .detail-card{
-            width: 100%;
-        }
+@media screen and (max-width: 760px) {
+  .articledetail-page {
+    .detail-card {
+      width: 100%;
     }
-    aside {
-        display: none;
-    }
+  }
+  aside {
+    display: none;
+  }
 }
 </style>
--- src/views/Write/article.vue
+++ src/views/Write/article.vue
@@ -1,10 +1,59 @@
 <template>
-  <div>
-    我要写文章
+  <div class="Write">
+    <form action="#" class="write_form">
+      <div class="write_group">
+        <label for="title">标题</label>
+        <input name="title" type="text" />
+      </div>
+      <div class="write_group">
+        <label for="describe">摘要</label>
+        <input name="describe" type="text" />
+      </div>
+      <div class="write_group">
+        <label for="type">归属菜单</label>
+        <select>
+          <option>笔记</option>
+          <option>总结</option>
+        </select>
+      </div>
+      <div class="write_group">
+        <label for="describe">标签</label>
+      </div>
+      <div>
+        <h4>内容</h4>
+        <mavon-editor v-model="commentContent" ref="md" @change="changeComment" style="min-height: 600px" />
+      </div>
+    </form>
   </div>
 </template>
 <script>
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 export default {
-
+  components: {
+    mavonEditor,
+  },
+  data() {
+    return {
+      commentContent: '', // 输入的markdown
+      conmentHtml: '',    // 及时转的html
+    }
+  },
+  methods: {
+    // 所有操作都会被解析重新渲染
+    changeComment(value, render) {
+      console.log(value);
+      console.log(render);
+      // render 为 markdown 解析后的结果[html]
+      this.conmentHtml = render;
+    },
+    // 提交
+    submitComment() {
+      console.log(this.commentContent);
+      console.log(this.conmentHtml);
+    }
+  }
 }
 </script>
+<style lang="scss" scoped>
+</style>
